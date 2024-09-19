28f632f81d67c9ebc9094074ca19ef801ceb2b45
karinanurarieff <karinaarieffin68138@gmail.com>
2024-09-19 15:04:57 +0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+	public static void main(String[] args) { 
+		System.out.println("Nama: Karina Nur Arieffin");
+		System.out.println("NIM: 60706240008");
+		System.out.println("Kelas: D3IF4801");
+	}
+}
